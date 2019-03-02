3171f7b7ab3d9304f410a0e3bd0f0c96db48f1a4
jamessynge <james.synge@gmail.com>
2019-03-02 17:56:54 -0500

    Added use of EthernetBonjour library to RainSensorWebServer.
    Program now advertises itself with host name "rainsensor.local",
    allowing compatible OS's to locate it. Learn more by reading
    about mDNS (UDP Multicast based DNS) and ZeroConf.
    
    Starts by trying to get an address from DHCP, then falling
    back to a fixed IP address, allowing it to keep working on
    isolated networks (e.g. just the Arduino and a Raspberry Pi).
    
    Lots of printing is included, but that drives up the size of
    the sketch a bit.

--- RainSensorWebServer.ino
+++ RainSensorWebServer.ino
@@ -1,11 +1,27 @@
 /*
    This is adapted from the Arduino example:
          Ethernet > BarometricPressureWebServer
-   Intent is to read from a Hydreon Rain Gauge Model RG-11
+   Intent is to read from a Hydreon Rain Gauge Model RG-11.
+
+   For now though it is a demo of how to work with the Arduino
+   Ethernet and EthernetBonjour libraries, with lots of printed
+   status.
+
+   TODO: Add use of EEPROM library, storing a generated MAC address
+   on first run, and if DHCP fails, storing a generated IP address.
+   Ideally we'd have some means of detecting the IP addresses in use
+   on the local network segment (e.g. by using Promiscuous mode), but
+   it doesn't appear that the Arduino Ethernet library supports this. 
+
+   Author: James Synge
 */
 
+// This is the Arduino "standard" Ethernet 2.0.0 library (or later).
 #include <Ethernet.h>
 
+// I've forked this library just so that I'm sure I can find it:
+// https://github.com/jamessynge/EthernetBonjour
+#include <EthernetBonjour.h>
 
 // Assign a MAC address for the Ethernet controller. Since the Arduino
 // doesn't have its own MAC address assigned at the factory, you must
@@ -51,16 +67,18 @@ byte mac[] = {
   0x52, 0xC4, 0x58, 0xC7, 0x2E, 0x81
 };
 
-// assign an IP address for the controller:
-IPAddress ip(192, 168, 86, 251);
+// If we can't find a DHCP server to assign an address to us, we'll
+// fallback to this static address.
+IPAddress static_ip(192, 168, 86, 251);
+bool using_dhcp = true;
 
 // Initialize the Ethernet server library
 // with the IP address and port you want to use
 // (port 80 is default for HTTP):
 EthernetServer server(80);
 
-float temperature = 0.0;
-long pressure = 0;
+float temperature;
+float pressure;
 long lastReadingTime = 0;
 
 void setup() {
@@ -73,17 +91,22 @@ void setup() {
   long startTime = millis();
 
   // Open serial communications and wait for port to open, but not forever.
+  // By waiting, we help our debugging because we then have a chance for
+  // the Arduino IDE Serial Monitor to start reading from the Arduino
+  // before too many messages have been sent.
   Serial.begin(9600);
   // Wait for serial port to connect. Needed for native USB port only.
-  while (!Serial && (millis() - startTime) < 120UL * 1000) {
+  while (!Serial && (millis() - startTime) < 10 * 1000UL) {
     blink(200);
   }
 
-  // Allow some time (250 ms) after powerup and sketch start, for the
-  // Wiznet W5100 Reset IC to release and come out of reset.
-  while ((millis() - startTime) < 250) {
-    blink(100);
-  }
+  // This delay does NOT seem necessary because the Ethernet hardware
+  // library has a long delay inside the Ethernet.begin call.
+  //  // Allow some time (250 ms) after powerup and sketch start, for the
+  //  // Wiznet W5100 Reset IC to release and come out of reset.
+  //  while ((millis() - startTime) < 250) {
+  //    blink(100);
+  //  }
 
   Serial.println("Setting up Ethernet pins");
 
@@ -96,100 +119,135 @@ void setup() {
   //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
 
   Serial.println("Setting up Ethernet library");
-
-  // start the Ethernet connection
-  Ethernet.begin(mac, ip);
-
-  // Check for Ethernet hardware present
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    while (true) {
-      Serial.println("Ethernet shield was not found.");
-      Serial.println("Sorry, can't run without hardware. :(");
-      Serial.println();
-      delay(1000); // do nothing, no point running without Ethernet hardware
-    }
-  }
-  Serial.println("Found ethernet hardware");
-
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Ethernet cable is not connected.");
+  if (Ethernet.begin(mac)) {
+    using_dhcp = true;
+    Serial.print("DHCP successful, assigned address:");
+    Serial.println(Ethernet.localIP());
   } else {
-    Serial.println("Ethernet link is connected");
+    using_dhcp = false;
+    Serial.print("Unable to get an IP address via DHCP. Using fixed address: ");
+    Serial.println(static_ip);
+    Ethernet.setLocalIP(static_ip);
+
+    // Assume that the subnet is a /24, with this mask:
+    IPAddress subnet(255, 255, 255, 0);
+    Ethernet.setSubnetMask(subnet);
+
+    // Assume that the gateway is on the same subnet, but with the
+    // last byte of the address being '1':
+    IPAddress gateway = static_ip;
+    gateway[3] = 1;
+    Ethernet.setGatewayIP(gateway);
+  }
+
+  switch(Ethernet.hardwareStatus()) {
+    case EthernetNoHardware:
+      while (true) {
+        Serial.println("Ethernet hardware was not found!");
+        Serial.println("Sorry, can't run without it. :(");
+        Serial.println();
+        delay(1000); // do nothing, no point running without Ethernet hardware
+      }
+
+    case EthernetW5100:
+      Serial.println("W5100 Ethernet controller detected.");
+      break;
+
+    case EthernetW5200:
+      Serial.println("W5200 Ethernet controller detected.");
+      break;
+
+    case EthernetW5500:
+      Serial.println("W5500 Ethernet controller detected.");
+      break;
+
+    default:
+      Serial.print("Other Ethernet controller detected: ");
+      Serial.println(Ethernet.hardwareStatus());
+      break;
+  }
+
+  printChangedLinkStatus();
+
+  if (EthernetBonjour.begin("rainsensor")) {
+    Serial.println("Advertising name 'rainsensor.local'");
+  } else {
+    Serial.println("EthernetBonjour.begin FAILED!");
   }
 
   // start listening for clients
   server.begin();
 
-  // give the sensor and Ethernet shield time to set up:
+  // Give the sensor and Ethernet shield time to set up:
+  // NOT SURE THIS IS NECESSARY AT ALL.
+  startTime = millis();
+  
   delay(1000);
 
-  // Silly pseudo readings.
-  unsigned long t1 = millis();
-  unsigned long t2 = micros();
-  unsigned long a0 = analogRead(0);
-  unsigned long a1 = analogRead(1);
-  unsigned long a2 = analogRead(2);
-  unsigned long a3 = analogRead(3);
-  unsigned long a4 = analogRead(4);
-  unsigned long a5 = analogRead(5);
-  unsigned long seed = t1 + t2 + a0 + a1 + a2 + a3 + a4 + a5;
-  Serial.print("Seed inputs: ");
-  Serial.print(t1);
-  Serial.print(", ");
-  Serial.print(t2);
-  Serial.print(", ");
-  Serial.print(a0);
-  Serial.print(", ");
-  Serial.print(a1);
-  Serial.print(", ");
-  Serial.print(a2);
-  Serial.print(", ");
-  Serial.print(a3);
-  Serial.print(", ");
-  Serial.print(a4);
-  Serial.print(", ");
-  Serial.print(a5);
-  Serial.println();
-  Serial.print("Seed: ");
-  Serial.println(seed);
-  randomSeed(seed);
+  // Wait for serial port to connect. Needed for native USB port only.
+  while (!Serial && (millis() - startTime) < 10 * 1000UL) {
+    blink(200);
+  }
 
+  // SOLELY for this demo, without real hardware, initialize the random
+  // number generator's seed.
+  seedRNG();
 
-  temperature = random(-400, 1200) / 10.0;
-  pressure = random(800, 1100);
-
+  readFakeSensors();
 }
 
 unsigned long nextBlink = 0;
 bool ledIsOn = false;
 
+void printChangedLinkStatus() {
+  static bool first = true;
+  static EthernetLinkStatus last_status = Unknown;
+
+  EthernetLinkStatus status = Ethernet.linkStatus();
+  if (!first && status == last_status) {
+    return;
+  }
+  first = false;
+  last_status = status;
+  Serial.print("Link status: ");
+  switch (status) {
+    case Unknown:
+      Serial.println("Unknown. Link status detection is only available with W5200 and W5500.");
+      break;
+    case LinkON:
+      Serial.println("ON");
+      break;
+    case LinkOFF:
+      Serial.println("OFF");
+      break;
+  }
+}
+
 void loop() {
+  printChangedLinkStatus();
+
+  // If got address via DHCP, this will renew the lease.
+  if (using_dhcp) {
+    switch (Ethernet.maintain()) {
+      case 1: // Renew failed
+      case 3: // Rebind failed
+        Serial.println("WARNING! lost our DHCP assigned address!");
+        // MIGHT want to just return at this point, since the rest won't work.
+    }
+  }
+
+  // This actually runs the Bonjour module. YOU HAVE TO CALL THIS PERIODICALLY,
+  // OR NOTHING WILL WORK! Preferably, call it once per loop().
+  EthernetBonjour.run();
+
   // check for a reading no more than once a second.
   if (millis() - lastReadingTime > 1000) {
-    Serial.println("We should read from our FAKE sensor!");
-
-    temperature += random(-100, 100) / 100.0;
-    pressure += random(-100, 100) / 100.0;
-
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.println(" degrees F");
-    Serial.print("Pressure: ");
-    Serial.print(pressure);
-    Serial.println(" hPa");
-    Serial.println();
-
+//    Serial.println("We should read from our FAKE sensor!");
+    readFakeSensors();
     lastReadingTime = millis();
   }
 
   blink(1000);
-
-  // Show a heartbeat.
-  if (false && nextBlink <= millis()) {
-    nextBlink += 1000;
-    digitalWrite(LED_BUILTIN, ledIsOn ? LOW : HIGH);
-    ledIsOn = !ledIsOn;
-  }
 
   // listen for incoming Ethernet connections:
   listenForEthernetClients();
@@ -274,4 +332,73 @@ void listenForEthernetClients() {
     // close the connection:
     client.stop();
   }
+}
+
+void seedRNG() {
+  // Silly pseudo readings.
+  unsigned long t1 = millis();
+  unsigned long t2 = micros();
+  unsigned long a0 = analogRead(0);
+  unsigned long a1 = analogRead(1);
+  unsigned long a2 = analogRead(2);
+  unsigned long a3 = analogRead(3);
+  unsigned long a4 = analogRead(4);
+  unsigned long a5 = analogRead(5);
+  uint32_t ip1 = Ethernet.localIP();
+  uint32_t ip2 = Ethernet.gatewayIP();
+  uint32_t ip3 = Ethernet.subnetMask();
+  uint32_t ip4 = Ethernet.dnsServerIP();
+  Serial.print("Seed inputs: ");
+  Serial.print(t1);
+  Serial.print(", ");
+  Serial.print(t2);
+  Serial.print(", ");
+  Serial.print(ip1);
+  Serial.print(", ");
+  Serial.print(ip2);
+  Serial.print(", ");
+  Serial.print(ip3);
+  Serial.print(", ");
+  Serial.print(ip4);
+  Serial.print(", ");
+  Serial.print(a0);
+  Serial.print(", ");
+  Serial.print(a1);
+  Serial.print(", ");
+  Serial.print(a2);
+  Serial.print(", ");
+  Serial.print(a3);
+  Serial.print(", ");
+  Serial.print(a4);
+  Serial.print(", ");
+  Serial.print(a5);
+  Serial.println();
+  unsigned long seed = t1 + t2 + ip1 + ip2 + ip3 + ip4 + a0 + a1 + a2 + a3 + a4 + a5;
+  Serial.print("Seed: ");
+  Serial.println(seed);
+  randomSeed(seed);
+}
+
+void readFakeSensors() {
+  static bool first = true;
+
+  float t = random(-400, 1200) / 10.0;
+  float p = random(800, 1100);
+
+  if (first) {
+    first = false;
+    temperature = t;
+    pressure = p;
+  } else {
+    temperature = (temperature * 99 + t) / 100;
+    pressure = (pressure * 99 + p) / 100;
+  }
+
+//  Serial.print("Temperature: ");
+//  Serial.print(temperature);
+//  Serial.println(" degrees F");
+//  Serial.print("Pressure: ");
+//  Serial.print(pressure);
+//  Serial.println(" hPa");
+//  Serial.println();
 }
